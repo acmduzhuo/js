de69bee7483a0ea528322bcff861fd732dbdee7b
独酌 <1638388435@qq.com>
2019-08-05 16:25:17 +0800

    update

---
+++ string.js
@@ -0,0 +1,30 @@
+// alert('I\'m \"OK\"!');
+//alert('\\ a \/');
+// alert('\x41');//A
+//alert('\u4e2d\u6587');//中文
+// console.log(`多行
+// 字符串
+// 测试`);
+// var name = '小明';
+// var age = 4;
+// var message = '你好'+ ','+ name + '今年' + age + '岁了';
+// console.log(message);//perfect
+//'use strict';
+// var a = 'ABC';
+// console.log(a);
+// var name = '小明';
+// var age = 4;
+// console.log(`你好，${name}今年${age}岁了`);
+// var s = 'Hello World!';
+// console.log(s.length);
+// console.log(s[0]);
+// console.log(s[20]);
+var s = 'Test';
+// s[0] = 'X';
+//console.log(s.toUpperCase());
+// var ss = s.toLowerCase();
+// console.log(ss);
+// var n = s.indexOf('T')
+// console.log(n);
+var a = s.substring(1);
+console.log(a);
