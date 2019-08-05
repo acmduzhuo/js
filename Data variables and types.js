da0e5d43b45a4d97ba984cc3873a5ea0f0056075
独酌 <1638388435@qq.com>
2019-08-05 15:35:50 +0800

    update

---
+++ Data variables and types.js
@@ -0,0 +1,42 @@
+// alert('不知道会不会再乱码！');
+// alert(2>5);
+// var  age = 15;
+// if(age >= 18){
+//     alert('adult');
+// }
+// else{
+//     alert('teenager');
+// }
+// alert(1 === 1);
+//alert(NaN === NaN);//可怕的事情还是发生了，它居然显示“false”
+//alert(1/3 === (1 - 2/3));//这是一个false
+//alert(Math.abs(1/3 - (1 - 2/3)) < 0.0000001);
+// var arr = [1, 2, 3.14, 'Hello', null, true];
+// alert(arr[0]);
+// var person = {
+//     name: 'Bob',
+//     age: 20,
+//     tags: ['js', 'web', 'mobile'],
+//     city: 'Beijing',//这是一个字符串，记得加单引号
+//     hasCar: true,
+//     zipcode: null
+// };
+// alert(person.age);
+// var a;
+// var $b = 1;
+// var s_007 = '007';
+// var Answer = true;
+// var t = null;
+// alert(a);
+// alert($b);
+// alert(s_007);
+// alert(Answer);
+// alert(t);
+// var a = 123;
+// a = 'ABC';
+// alert(a);
+// var x = 100;
+// console.log(x);//强烈推荐console.log()，胜过弹窗式alert
+// 'use strict';
+// abc = 'Hello, world';
+// console.log(abc);
