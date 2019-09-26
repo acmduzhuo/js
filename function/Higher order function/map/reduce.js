a238511d4581c3122d1fd512da1e48e122dcaba6
duzhuo <1638388435@qq.com>
2019-09-26 22:00:24 +0800

    update

---
+++ function/Higher order function/map/reduce.js
@@ -0,0 +1,39 @@
+'use strict';
+// function pow(x) {
+//     return x*x;
+// };
+// var arr = [1, 2, 3, 4, 5];
+// var sum = arr.map(pow);//调用平方pow函数
+// console.log(sum);
+// function jiafa(x) {
+//     var y = (x + 2) * 34;
+//     return y;
+// };
+// var a = 2;
+// var sum = a.map(jiafa);
+// console.log(sum);//map只对数组进行操作
+// function f(x) {
+//     return x * x;
+// };
+//  var arr = [1, 2, 3, 4, 5];
+// var result = [];
+// for(var i=0; i<arr.length; i++){
+//     result.push(f(arr[i]));
+// }
+// console.log(result);
+// var arr = [1, 2, 3, 4, 5];
+// console.log(arr.map(String));//转化为字符串
+// var sum = arr.map(f);
+// console.log(sum);
+var arr = [1, 2, 3, 4, 5];
+// function f(x,y) {
+//     return x + y;
+// }
+// var sum = arr.reduce(f);
+// console.log(sum);//输出为15，求和函数。如果是只传送x值，会输出1。
+// function f(x, y) {
+//     return 10 * x + y;
+// }
+// var sum = arr.reduce(f);
+// console.log(sum);//利用f(f(x1, x2), x3)
+
