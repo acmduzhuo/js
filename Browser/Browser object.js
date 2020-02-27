d8641bd569a82a95573e94ee275de2afab505c8e
acmduzhuo <1638388435@qq.com>
2020-02-27 12:14:34 +0800

    update Brower importance:window navigater screen location document

---
+++ Browser/Browser object.js
@@ -0,0 +1,33 @@
+//window
+`use strict`;
+//console.log('window inner size: '+ window.innerWidth + ' x ' + window.innerHeight);
+//innerWidth 浏览器内部宽度
+//innerHifght 浏览器内部高度
+//navigator
+//appName 浏览器名称
+//appVersion 浏览器版本
+//language 浏览器设置的语言
+//platform 操作系统
+//userAgent 浏览器设定的User-Agent字符串
+//screen
+//width 屏幕宽度
+//height 屏幕高度
+//colorDepth 返回颜色位数
+//location
+//href 获取URL值 http://www.example.com:8080/path/index.html?a=1&b=2#TOP
+//protocol 'http'
+//host 'www.example.com'
+//port '8080'
+//pathname 'path/index.html'
+//search '?a=1&b=2'
+//hash 'TOP'
+//assign 加载界面
+//reload 重新加载界面
+// if (confirm('重新加载当前页' + location.href + '?')) {
+//     location.reload();
+// } else {
+//     location.assign('/'); // 设置一个新的URL地址
+// }
+//document
+//document对象表示当前界面，由于HTML在浏览器中以DOM形式表示为树结构，document对象就是整个DOM树的根节点
+//title 用于改变title属性
