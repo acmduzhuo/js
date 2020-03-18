e098741a399d031e5b03602b84fe29ffe37ef485
acmduzhuo <1638388435@qq.com>
2020-03-18 13:43:03 +0800

    update jQuery DOM  importance:click

---
+++ jQuery/event.js
@@ -0,0 +1,9 @@
+var a = $('#test-link');
+a.mouseenter(function () {
+    alert('Hello!');
+});
+click//鼠标单击时触发
+dblclick//鼠标双击时触发
+mouseenter//鼠标进入时触发
+mouseleave//鼠标移出时触发
+hover//鼠标进入和退出时触发两个函数，相当于mouseenter和mouseleave两个函数
